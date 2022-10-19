4a142ffc0b77b7dc27264f6fc0c54ec6c58a1022
Harsh Tomar <letmiphone@gmail.com>
2022-10-19 10:29:47 +0530

    Update Loan.java

--- Loan.java
+++ Loan.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package library;
 
 import java.util.*;
@@ -12,10 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Minahil Imtiaz
- */
 public class Loan {
 
     private int loanId;
