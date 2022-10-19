b255ea295c510019df3278746a24d39843b041a6
Harsh Tomar <letmiphone@gmail.com>
2022-10-19 10:23:23 +0530

    Update Clerk.java

--- Clerk.java
+++ Clerk.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package library;
 
-/**
- *
- * @author Minahil Imtiaz
- */
 public class Clerk extends Staff {
 
     Clerk(int user_id, String user_name, char gender) {
