d87516eeab94538d7a5e2d549139f7dc02a6db0b
Harsh Tomar <letmiphone@gmail.com>
2022-10-19 10:21:14 +0530

    Update Books.java

--- Books.java
+++ Books.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package library;
 
 import java.util.*;
@@ -11,10 +7,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- *
- * @author Minahil Imtiaz
- */
 public class Books {
 
     private int book_id;
